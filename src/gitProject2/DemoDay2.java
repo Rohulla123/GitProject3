eaaa7f4527312d67b6d38082f0ea9b9f2806db94
Rohulla123 <rohullah.asify@gmail.com>
2023-07-29 11:10:56 -0400

    Initiated commit

---
+++ src/gitProject2/DemoDay2.java
@@ -0,0 +1,20 @@
+package gitProject2;
+
+public class DemoDay2 {
+public static void main(String[] args) {
+	
+	
+	System.out.println("Test");
+}
+
+ // add method
+public static void add( int a, int b) {
+	System.out.println(a+b);
+	
+}
+
+public static void substract(int a, int b) {
+	
+	System.out.println(a-b);
+}
+}
